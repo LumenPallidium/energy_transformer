90470f3ece6c71564751581127fcbdf490947b8f
Nameless <keirhavel@live.com>
2023-05-29 13:06:42 -0700

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name = "energy_transformer",
-    version = "0.1.2",
+    version = "0.1.3",
     author = "Keir Havel",
     author_email = "keirhavel@live.com",
     description = "Pytorch implementation of an energy transformer.",
